a5c39d2560224aba36fa8561e71b69b00cbffd54
Carlos Eduardo Cortes Fernandez <90275964+ChaZzT-2049@users.noreply.github.com>
2022-05-25 10:34:39 -0500

    service worker correction

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 if('serviceWorker' in navigator){
-    navigator.serviceWorker.register('/serviceworker.js')
+    navigator.serviceWorker.register('https://chazzt-2049.github.io/curriculum-pwa/serviceworker.js')
     .then(reg => console.log('Registro sw exitoso', reg))
     .catch(error => console.warn(error))
 }
