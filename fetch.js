540ee6ab5abd7359a98fec3d828139a7307068cc
ChaZzT-2049 <2005041@utrivieramaya.edu.mx>
2023-09-25 16:23:29 -0500

    actualizacion de estilos, datos, foto del curriculum

--- fetch.js
+++ fetch.js
@@ -7,120 +7,88 @@ function loadInfo() {
         .catch((error) => console.log(error));
 
     const showData = (data) => {
-        console.log(data)
-        let datos = "";
-        let lenguajes = "";
-        let databases = "";
-        let idiomas = "";
-        let informacion = "";
-        let formacion = "";
-        let trabajos = "";
-        let referencias = "";
-        datos += `
-            <li>
-                Domicilio: ${data[0].domicilio}
-            </li>
-            <li>
-                Celurlar: ${data[0].numero}
-            </li>
-            <li>
-                Correo: ${data[0].correo}
-            </li>
-            <li>
-                Edad: ${data[0].edad}
-            </li>
-            <li>
-                Fecha de Nacimiento: ${data[0].fechaNacimiento}
-            </li>
-            <li>
-                Lugar de Nacimiento: ${data[0].lugarNacimiento}
-            </li>
-            <li>
-                Nacionalidad: ${data[0].nacionalidad}
-            </li>
-        `;
-        lenguajes += `
-            <li>
-                PHP: ${data[1].php}
-            </li>
-            <li>
-                JavaScript: ${data[1].javascript}
-            </li>
-            <li>
-                HTML: ${data[1].html}
-            </li>
-            <li>
-                CSS: ${data[1].css}
-            </li>
-            <li>
-                Python: ${data[1].python}
-            </li>
-        `;
-        databases += `
-            <li>
-                MySQL: ${data[2].mysql}
-            </li>
-        `;
-        idiomas += `
-            <li>
-                Español: ${data[3].español}
-            </li>
-            <li>
-                Ingles: ${data[3].ingles}
-            </li>
-        `;
-        informacion += `
-            <li>
-                Nombre: ${data[4].nombre}
-            </li>
-            <li>
-                ¿Quién Soy? ${data[4].quienSoy}
-            </li>
-            <li>
-                Puesto Solicitado: ${data[4].puestoSolicitado}
-            </li>
-        `;
-        formacion += `
-            <li>
-                Secundaria: ${data[5].secundaria}
-            </li>
-            <li>
-                Preparatoria: ${data[5].preparatoria}
-            </li>
-            <li>
-                Universidad: ${data[5].universidad}
-            </li>
-        `;
-        trabajos += `
-            <li>
-                Empresa: ${data[6].empresa}
-            </li>
-            <li>
-                Puesto: ${data[6].puesto}
-            </li>
-            <li>
-                Tiempo: ${data[6].tiempo}
-            </li>
-        `;
-        referencias += `
-            <li>
-                Nombre: ${data[7].nombreRef}
-            </li>
-            <li>
-                Puesto: ${data[7].cargo}
-            </li>
-            <li>
-                Celular: ${data[7].celular}
-            </li>
-        `;
-        document.getElementById('datosp').innerHTML = datos;
-        document.getElementById('lp').innerHTML = lenguajes;
-        document.getElementById('db').innerHTML = databases;
-        document.getElementById('im').innerHTML = idiomas;
-        document.getElementById('inf').innerHTML = informacion;
-        document.getElementById('fa').innerHTML = formacion;
-        document.getElementById('tb').innerHTML = trabajos;
-        document.getElementById('ref').innerHTML = referencias;
+        console.log(data[0])
+
+        //main
+
+        const nombre = document.querySelector("#nombre");
+        nombre.textContent = data[0].datos.nombre
+
+        const desc = document.querySelector("#desc")
+        desc.textContent = data[0].datos.desc
+
+        const perfil = document.querySelector("#perfil")
+        perfil.textContent = data[0].datos.perfil
+
+        const educacion = document.querySelector("#educacion")
+        data[0].educacion.forEach(entidad => {
+            const div = document.createElement("div")
+            div.innerHTML = `
+                <h4>${entidad.entidad}</h4>
+                <span>${entidad.fechaEstado}</span>
+                <p>${entidad.carrera}</p>
+            `;
+            educacion.appendChild(div)
+        })
+
+        const experiencia = document.querySelector("#experiencia")
+        data[0].experiencia.forEach(exp => {
+            const div = document.createElement("div")
+            div.innerHTML = `
+                <h4>${exp.lugar}</h4>
+                <b>${exp.fecha}</b>
+                <h5>${exp.puesto}</h5>
+                <p>${exp.desc}</p>
+            `;
+            experiencia.appendChild(div)
+        })
+
+        const competencias = document.querySelector("#competencias")
+        data[0].competencias.forEach(competencia => {
+            const li = document.createElement("li")
+            li.textContent = competencia.competencia
+            competencias.appendChild(li)
+        })
+
+        //aside
+
+        const titulo = document.querySelector("#titulo")
+        titulo.textContent = data[0].datos.titulo
+
+        const tel = document.querySelector("#tel")
+        tel.textContent = data[0].datos.tel
+
+        const correo = document.querySelector("#correo")
+        correo.textContent = data[0].datos.correo
+        correo.href = `mailto:${data[0].datos.correo}`
+
+        const idiomas = document.querySelector("#idiomas")
+        data[0].conocimientos.idiomas.forEach(idioma => {
+           const li = document.createElement("li")
+           li.textContent = `${idioma.idioma}: ${idioma.nivel}`
+           idiomas.appendChild(li)
+        });
+
+        const programas = document.querySelector("#programas")
+        data[0].conocimientos.programas.forEach(programa => {
+            const li = document.createElement("li")
+            li.textContent = programa.nombre
+            programas.appendChild(li)
+        })
+
+        const programacion = document.querySelector("#programacion")
+        data[0].conocimientos.programacion.forEach(lenguaje => {
+            const li = document.createElement("li")
+            li.textContent = lenguaje.lenguaje
+            programacion.appendChild(li)
+        })
+
+        const dbs = document.querySelector("#dbs")
+        data[0].conocimientos.basesDatos.forEach(db => {
+            const li = document.createElement("li")
+            li.textContent = db.nombre
+            dbs.appendChild(li)
+        })
     }
 }
 
